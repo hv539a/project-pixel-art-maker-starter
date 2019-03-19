10c3ceb6dbf9a80299a9ac3eb3d99b8202ca94b3
hv539a <47986334+hv539a@users.noreply.github.com>
2019-03-19 06:27:52 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,7 +1,11 @@
 // Select color input
 // Select size input
+var height, width, color;
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+    event.preventDefault
+  
 
 function makeGrid() {
 
